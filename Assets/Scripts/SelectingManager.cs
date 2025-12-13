fd9b44b208e260a9d0cc1a20a84c9950bdcfad3e
ko-yawata <183326521+ko-yawata@users.noreply.github.com>
2025-12-13 18:00:42 +0900

    昼飯会話json追加

---
+++ Assets/Scripts/SelectingManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+public class SelectingManager : MonoBehaviour
+{
+    [SerializeField] private List<Button> a;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
